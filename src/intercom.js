e9cd9087d719b1131769f957009dc57ecd5b4582
Tony Liu <80608451+liudonglinyahoo@users.noreply.github.com>
2024-09-26 13:45:26 -0500

    change bot

--- src/intercom.js
+++ src/intercom.js
@@ -3,7 +3,8 @@ import Intercom from '@intercom/messenger-js-sdk';
 
 export default function Component() {
   Intercom({
-    app_id: 'osdluh2g',
+    //app_id: 'osdluh2g',
+    app_id:zeoovopy',
   });
 
   return <div>Click on the app on the bottom right.</div>;
