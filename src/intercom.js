cf694b418e48b6d06b1c7671f48a9b4b3e81d755
Tony Liu <80608451+liudonglinyahoo@users.noreply.github.com>
2024-09-26 13:46:34 -0500

    Update intercom.js

--- src/intercom.js
+++ src/intercom.js
@@ -4,7 +4,7 @@ import Intercom from '@intercom/messenger-js-sdk';
 export default function Component() {
   Intercom({
     //app_id: 'osdluh2g',
-    app_id:zeoovopy',
+    app_id:'zeoovopy',
   });
 
   return <div>Click on the app on the bottom right.</div>;
